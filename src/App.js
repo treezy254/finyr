6785f4bb81accafa4c211de1b47459ee9676d8f5
painkiller254 <gachungasamuel53@gmail.com>
2023-01-12 20:44:32 +0300

    another one

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Landing from './pages/landing/Landing';
+import "./index.css"
+function App() {
+  return (
+    <div className="App">
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
